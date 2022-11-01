377bf578ae4636d272b3cbd4b416154fe39e7481
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2022-11-01 14:30:22 +0100

    Add files via upload

---
+++ Program/Program/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PictureBox_kontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Image = global::PictureBox_kontrola.Properties.Resources.senator_armstrong_screeching;
+            this.picBox.Location = new System.Drawing.Point(12, 12);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(348, 273);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            this.picBox.Visible = false;
+            this.picBox.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(93, 314);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(189, 54);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Prikazi sliku";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 397);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.picBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
